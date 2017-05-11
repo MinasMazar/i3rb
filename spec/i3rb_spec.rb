f5519be66ae95ab33edb6ea395d3bb2bef37d8ae
MinasMazar <minasmazar@gmail.com>
2017-05-11 21:11:58 +0200

    Initial commit: bundle gem i3rb

---
+++ spec/i3rb_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe I3rb do
+  it "has a version number" do
+    expect(I3rb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
